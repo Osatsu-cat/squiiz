cfd99ab6fc6af735eb5ba8b55e69089bb9659e7b
Osatsu-cat <moluca5460@gmail.com>
2019-08-17 15:46:35 +0900

    マイページview編集・問題詳細ページ追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require activestorage
 //= require jquery
 //= require jquery_ujs
+//= require popper
 //= require turbolinks
-//= require bootstrap
+// = require bootstrap
 //= require_tree .
--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -5,4 +5,35 @@ $(document).on('turbolinks:load', function(){
   //     $(this).addClass('mypage_tab-select');
   //   }
   // });
+
+  //選択中のドロップダウンメニューの表示
+  $('.dropdown-menu .dropdown-item').click(function(){
+    var visibleItem = $('.dropdown-toggle', $(this).closest('.dropdown'));
+    var order = $(this).val();
+    visibleItem.text(order);
+    if (order == '問題番号順'){
+      $('.asc').show();
+      $('.user').hide();
+    }else{
+      $('.asc').hide();
+      $('.user').show();
+    }
+  });
+
+  //マイページメミューの表示(スマホ)
+  $('.mypage_slide').on('click',function(){
+    if($('.fa-chevron-right').css('display') == 'inline-block'){
+      $(this).animate({'left': '100px'}, 300);
+      $('.mypage_bar').animate({'left': 0}, 300);
+      $(this).find('.fa-chevron-right').hide();
+      $(this).find('.fa-chevron-left').show();
+      console.log("GO");
+    }else{
+      $(this).animate({'left': 0 }, 300);
+      $('.mypage_bar').animate({'left': '-100px'}, 300);
+      $(this).find('.fa-chevron-left').hide();
+      $(this).find('.fa-chevron-right').show();
+      console.log("BACK");
+    }
+  });
 });
